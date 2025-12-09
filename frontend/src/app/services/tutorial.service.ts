e04205b75eed41d1c03212d9f32defea6c9ddc75
Sayindra Rathod <sahilrathodrk31@gmail.com>
2025-12-09 13:46:03 +0530

    Update tutorial.service.ts

--- frontend/src/app/services/tutorial.service.ts
+++ frontend/src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://13.203.207.188:8081/api/tutorials';
+const baseUrl = 'http://15.207.89.48:8081/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
